c7238396e060a86451cf8045964a31a4c84fc83c
Vitor Renan <vitorrenanalmeida@gmail.com>
2023-10-03 20:54:53 -0300

    Daily Commit

---
+++ ex000/index001.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Freeing my self from the curse!</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            print "Hello, world! \u{1F30E}";
+        ?>
+    </h1>
+    <p>
+    Let's try free ourselves from the curse!
+    </p>
+</body>
+</html>
