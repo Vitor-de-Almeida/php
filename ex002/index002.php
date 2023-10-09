18f327ac2d14969152d8c1ac707ead499fc9408a
Vitor Renan <vitorrenanalmeida@gmail.com>
2023-10-08 21:35:33 -0300

    Daily Commit

---
+++ ex002/index002.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1>Example of PHP</h1>
+    <?php
+        $name = "Gustavo";
+        $lastname = "Guanabara";
+        const COUNTRY = "United States Of America";
+        
+        
+        $name =  "Rafael";
+        $lastname = "Silva";
+        
+        $salary = 2500.75;
+
+        echo "My pleasure, $name $lastname! You live in ". COUNTRY, "...<br>";
+        print "Is true that your name is $name?<br>";
+        print "Your monthly salary is $salary?"
+    ?>
+</body>
+</html>
